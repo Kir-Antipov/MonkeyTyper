0e633519a35f208de29e566006b6b75d17164587
Kir-Antipov <kp_antipov@mail.ru>
2020-08-11 07:27:08 +0300

    "Ported" MaybeNullAttribute

---
+++ MonkeyTyper.Core/Compatibility/MaybeNullAttribute.cs
@@ -0,0 +1,7 @@
+﻿#if !NETSTANDARD2_1
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
+    internal class MaybeNullAttribute : Attribute { }
+}
+#endif
