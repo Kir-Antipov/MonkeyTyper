ca97b60139323a3f59eac5d95c0a1c33fcbab137
Kir-Antipov <kp_antipov@mail.ru>
2020-08-11 09:39:41 +0300

    Implemented empty mock of the DataRecord

---
+++ MonkeyTyper.Core/Data/EmptyDataRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MonkeyTyper.Core.Data
+{
+    internal class EmptyDataRecord : DataRecord
+    {
+        public override object? this[string name] => throw new IndexOutOfRangeException();
+        public override object? this[int i] => throw new IndexOutOfRangeException();
+
+        public override string[] PropertyNames => Array.Empty<string>();
+        public override int PropertyCount => 0;
+    }
+}
