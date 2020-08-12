3c162b3a72f56be6e2df2c77c9c400b65b8bc631
Kir-Antipov <kp_antipov@mail.ru>
2020-08-12 14:21:25 +0300

    Added interface for custom string formatting.

---
+++ MonkeyTyper.Core/Format/IStringFormatter.cs
@@ -0,0 +1,22 @@
+﻿using MonkeyTyper.Core.Data;
+using System;
+
+namespace MonkeyTyper.Core.Format
+{
+    /// <summary>
+    /// Defines a method that supports custom string formatting.
+    /// </summary>
+    public interface IStringFormatter
+    {
+        /// <summary>
+        /// Transforms record values to an equivalent string representation
+        /// using specified format and culture-specific formatting information.
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications.</param>
+        /// <param name="record">A value provider.</param>
+        /// <param name="formatProvider">An object that supplies format information about the current instance.</param>
+        /// <returns>Formatted string.</returns>
+        /// <exception cref="FormatException">Input string was not in a correct format.</exception>
+        string Format(string format, IDataRecord record, IFormatProvider formatProvider);
+    }
+}
