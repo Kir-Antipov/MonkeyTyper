45ae24a65582129b1fd670445c2632f146b2a22c
Kir-Antipov <kp_antipov@mail.ru>
2020-08-12 14:41:47 +0300

    Made base class for the IStringFormatter

---
+++ MonkeyTyper.Core/Format/StringFormatter.cs
@@ -0,0 +1,14 @@
+﻿using MonkeyTyper.Core.Data;
+using System;
+
+namespace MonkeyTyper.Core.Format
+{
+    /// <summary>
+    /// Base class for <see cref="IStringFormatter"/>.
+    /// </summary>
+    public abstract class StringFormatter : IStringFormatter
+    {
+        /// <inheritdoc cref="IStringFormatter.Format(string, IDataRecord, IFormatProvider)"/>
+        public abstract string Format(string format, IDataRecord record, IFormatProvider formatProvider);
+    }
+}
