0fea47f53815a80af7580ae741fd9c64545852c7
Kir-Antipov <kp_antipov@mail.ru>
2020-08-12 18:53:20 +0300

    Described interface of the plugin manager

---
+++ MonkeyTyper.Core/Plugins/IPluginManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MonkeyTyper.Core.Plugins
+{
+    /// <summary>
+    /// Describes methods for interacting with custom plugins.
+    /// </summary>
+    public interface IPluginManager
+    {
+        /// <summary>
+        /// Loads plugins from the default location.
+        /// </summary>
+        /// <returns>
+        /// Descriptors of services loaded from plugins.
+        /// </returns>
+        IEnumerable<ServiceDescriptor> Load();
+
+        /// <summary>
+        /// Loads plugins from the specified files.
+        /// </summary>
+        /// <param name="files">
+        /// File paths containing plugin assemblies.
+        /// </param>
+        /// <returns>
+        /// Descriptors of services loaded from plugins.
+        /// </returns>
+        /// <exception cref="FileNotFoundException">
+        /// One of the specified files was not found.
+        /// </exception>
+        IEnumerable<ServiceDescriptor> Load(IEnumerable<string> files);
+    }
+}
