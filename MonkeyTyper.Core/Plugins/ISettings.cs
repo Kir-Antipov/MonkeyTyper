7f97e22b0a0c44342d032a0d46fe1a8269f9cb96
Kir-Antipov <kp_antipov@mail.ru>
2020-08-12 20:44:43 +0300

    Described settings interface

---
+++ MonkeyTyper.Core/Plugins/ISettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonkeyTyper.Core.Plugins
+{
+    /// <summary>
+    /// Describes the available settings for the registered service.
+    /// </summary>
+    public interface ISettings : IDictionary<string, object?>
+    {
+        /// <summary>
+        /// Human readable name of the settings section.
+        /// </summary>
+        string DisplayName { get; }
+
+        /// <summary>
+        /// Unique and permanent identifier of this type.
+        /// </summary>
+        Guid Guid { get; }
+    }
+}
