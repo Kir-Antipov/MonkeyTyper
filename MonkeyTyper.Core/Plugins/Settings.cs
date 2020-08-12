11df7e3efdcef8059403ba0836942d9d0dd88b27
Kir-Antipov <kp_antipov@mail.ru>
2020-08-12 20:57:06 +0300

    Made base class for the ISettings

---
+++ MonkeyTyper.Core/Plugins/Settings.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace MonkeyTyper.Core.Plugins
+{
+    /// <summary>
+    /// Base class for <see cref="ISettings"/>.
+    /// </summary>
+    /// <remarks>
+    /// This class uses public read/write properties
+    /// of the inherited type as available settings.
+    /// <para></para>
+    /// Don't forget to mark the inherited type
+    /// with the <see cref="GuidAttribute"/>.
+    /// </remarks>
+    public abstract class Settings : ISettings
+    {
+        #region Var
+        private static Regex NameExtractor { get; } = new Regex(@"Settings?$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private Dictionary<string, PropertyInfo> Properties { get; }
+
+        /// <inheritdoc cref="ISettings.DisplayName"/>
+        public virtual string DisplayName { get; }
+
+        /// <inheritdoc cref="ISettings.Guid"/>
+        public virtual Guid Guid { get; }
+
+        /// <inheritdoc cref="IDictionary{string, object}.Keys"/>
+        public ICollection<string> Keys { get; }
+
+        /// <inheritdoc cref="IDictionary{string, object}.Values"/>
+        public ICollection<object?> Values => Properties.Values.Select(x => x.GetValue(this)).ToArray();
+
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.IsReadOnly"/>
+        public bool IsReadOnly => false;
+
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.Count"/>
+        public int Count => Properties.Count;
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Initialize a new instance of the <see cref="Settings"/> class.
+        /// </summary>
+        public Settings()
+        {
+            Type thisType = GetType();
+            DisplayName = NameExtractor.Replace(thisType.Name, x => string.Empty);
+            Guid = thisType.GUID;
+            Properties = thisType
+                .GetProperties()
+                .Where(x => x.CanRead && x.CanWrite && x.GetIndexParameters().Length == 0)
+                .ToDictionary(x => x.Name, StringComparer.InvariantCultureIgnoreCase);
+
+            Keys = Properties.Keys.ToList().AsReadOnly();
+        }
+        #endregion
+
+        #region Functions
+        /// <inheritdoc cref="IDictionary{string, object}.this[string]"/>
+        public object? this[string key]
+        {
+            get => TryGetValue(key, out object? value) ? value : throw new KeyNotFoundException();
+            set => Add(key, value);
+        }
+
+        /// <inheritdoc cref="IDictionary{string, object}.Add(string, object)"/>
+        public void Add(string key, object? value)
+        {
+            _ = key ?? throw new ArgumentNullException(nameof(key));
+
+            if (Properties.ContainsKey(key))
+                Properties[key].SetValue(this, value);
+            else
+                throw new NotSupportedException();
+        }
+
+        /// <inheritdoc cref="IDictionary{string, object}.ContainsKey(string)"/>
+        public bool ContainsKey(string key) => Properties.ContainsKey(key ?? throw new ArgumentNullException(nameof(key)));
+
+        /// <inheritdoc cref="IDictionary{string, object}.TryGetValue(string, out object)"/>
+        public bool TryGetValue(string key, out object? value)
+        {
+            _ = key ?? throw new ArgumentNullException(nameof(key));
+
+            if (Properties.TryGetValue(key, out PropertyInfo? property))
+            {
+                value = property.GetValue(this);
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        /// <inheritdoc cref="IDictionary{string, object}.Remove(string)"/>
+        public bool Remove(string key) => throw new NotSupportedException();
+
+
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.Add(KeyValuePair{string, object})"/>
+        void ICollection<KeyValuePair<string, object?>>.Add(KeyValuePair<string, object?> item) => Add(item.Key, item.Value);
+
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.Contains(KeyValuePair{string, object})"/>
+        bool ICollection<KeyValuePair<string, object?>>.Contains(KeyValuePair<string, object?> item) => TryGetValue(item.Key, out object? value) && Equals(item.Value, value);
+        
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.Remove(KeyValuePair{string, object})"/>
+        bool ICollection<KeyValuePair<string, object?>>.Remove(KeyValuePair<string, object?> item) => throw new NotSupportedException();
+        
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.Clear"/>
+        void ICollection<KeyValuePair<string, object?>>.Clear() => throw new NotSupportedException();
+        
+        /// <inheritdoc cref="ICollection{KeyValuePair{string, object}}.CopyTo(KeyValuePair{string, object}[], int)"/>
+        void ICollection<KeyValuePair<string, object?>>.CopyTo(KeyValuePair<string, object?>[] array, int arrayIndex)
+        {
+            foreach (var pair in this)
+            {
+                if (arrayIndex >= array.Length)
+                    throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+
+                array[arrayIndex++] = pair;
+            }
+        }
+
+
+        /// <inheritdoc cref="IEnumerable.GetEnumerator"/>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc cref="IEnumerable{KeyValuePair{string, object}}.GetEnumerator"/>
+        public IEnumerator<KeyValuePair<string, object?>> GetEnumerator() =>
+            Properties.Select(x => new KeyValuePair<string, object?>(x.Key, x.Value.GetValue(this))).GetEnumerator();
+        #endregion
+    }
+}
