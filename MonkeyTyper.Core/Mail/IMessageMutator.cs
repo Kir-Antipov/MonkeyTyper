1adba0ae8e0fddd933008abdfef254a04d93c70c
Kir-Antipov <kp_antipov@mail.ru>
2020-08-13 11:49:51 +0300

    Added interface for message mutators

---
+++ MonkeyTyper.Core/Mail/IMessageMutator.cs
@@ -0,0 +1,23 @@
+﻿using MonkeyTyper.Core.Data;
+
+namespace MonkeyTyper.Core.Mail
+{
+    /// <summary>
+    /// Describes a transformation of a message object.
+    /// </summary>
+    public interface IMessageMutator
+    {
+        /// <summary>
+        /// Transforms the <see cref="IMessageBuilder"/>
+        /// into a new form.
+        /// </summary>
+        /// <param name="message">
+        /// The <see cref="IMessageBuilder"/> instance.
+        /// </param>
+        /// <param name="record">Data source record.</param>
+        /// <returns>
+        /// Transformed <see cref="IMessageBuilder"/> instance.
+        /// </returns>
+        IMessageBuilder Mutate(IMessageBuilder message, IDataRecord record);
+    }
+}
