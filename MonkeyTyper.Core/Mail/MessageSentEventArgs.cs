1d6e168d8e85b3ccc04283eb3312db29dbb7d7a9
Kir-Antipov <kp_antipov@mail.ru>
2020-08-13 14:27:17 +0300

    Added event args for the MessageSent event

---
+++ MonkeyTyper.Core/Mail/MessageSentEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using MimeKit;
+using System;
+
+namespace MonkeyTyper.Core.Mail
+{
+    /// <summary>
+    /// The <see cref="EventArgs"/> for <see cref="IMailClient.MessageSent"/>.
+    /// </summary>
+    public class MessageSentEventArgs : EventArgs
+    {
+        /// <summary>
+        /// The message.
+        /// </summary>
+        public MimeMessage Message { get; }
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="MessageSentEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MessageSentEventArgs(MimeMessage message) => Message = message ?? throw new ArgumentNullException(nameof(message));
+    }
+}
