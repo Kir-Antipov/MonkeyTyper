1a70d3ebf115b53d02dc73d60f8ff959bac02d1c
Kir-Antipov <kp_antipov@mail.ru>
2020-08-13 12:40:39 +0300

    Added ability to create ITransferProgress from delegate

---
+++ MonkeyTyper.Core/Mail/TransferProgress.cs
@@ -0,0 +1,16 @@
+﻿using MailKit;
+using System;
+
+namespace MonkeyTyper.Core.Mail
+{
+    internal class TransferProgress : ITransferProgress
+    {
+        private readonly Action<long, long> ReportImpl;
+
+        public TransferProgress(Action<long, long> report) => ReportImpl = report ?? throw new ArgumentNullException(nameof(report));
+
+        public void Report(long bytesTransferred, long totalSize) => ReportImpl(bytesTransferred, totalSize);
+
+        public void Report(long bytesTransferred) => ReportImpl(bytesTransferred, -1);
+    }
+}
