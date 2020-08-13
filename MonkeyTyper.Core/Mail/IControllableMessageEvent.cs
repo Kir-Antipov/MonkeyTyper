a5ae2f0ea89b01347171bef53ca68c13cdec79aa
Kir-Antipov <kp_antipov@mail.ru>
2020-08-13 13:11:20 +0300

    Added IControllableMessageEvent interface

---
+++ MonkeyTyper.Core/Mail/IControllableMessageEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MonkeyTyper.Core.Mail
+{
+    /// <summary>
+    /// Describes an <see cref="EventArgs"/> that can cancel
+    /// the sending of a message or terminate the sending method.
+    /// </summary>
+    public interface IControllableMessageEvent
+    {
+        /// <summary>
+        /// If this property is set to <see langword="true"/>,
+        /// the message won't be sent.
+        /// </summary>
+        bool Skip { get; set; }
+
+        /// <summary>
+        /// If this property is set to <see langword="true"/>,
+        /// the message won't be sent,
+        /// and the send method will be terminated.
+        /// </summary>
+        bool Stop { get; set; }
+    }
+}
