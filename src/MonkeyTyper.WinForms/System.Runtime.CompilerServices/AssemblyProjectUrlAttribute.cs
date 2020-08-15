74cf5916beba3a8ed379fae83c8308244722c2e8
Kir-Antipov <kp_antipov@mail.ru>
2020-08-15 08:39:29 +0300

    Added ProjectUrl and HelpUrl assembly attributes

---
+++ src/MonkeyTyper.WinForms/System.Runtime.CompilerServices/AssemblyProjectUrlAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false, Inherited = false)]
+    internal class AssemblyProjectUrlAttribute : Attribute
+    {
+        public string ProjectUrl { get; }
+
+        public AssemblyProjectUrlAttribute(string projectUrl) => ProjectUrl = projectUrl;
+    }
+}
