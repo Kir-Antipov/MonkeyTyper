4b1f78ce0dc524732486b3b8e717a1e88b604947
Kir-Antipov <kp_antipov@mail.ru>
2020-08-15 08:41:43 +0300

    Described interface for accessing project data

---
+++ src/MonkeyTyper.WinForms/Data/IProjectProperties.cs
@@ -0,0 +1,38 @@
+﻿namespace MonkeyTyper.WinForms.Data
+{
+    /// <summary>
+    /// Describes project properties.
+    /// </summary>
+    public interface IProjectProperties
+    {
+        /// <summary>
+        /// Project's copyright.
+        /// </summary>
+        string Copyright { get; }
+
+        /// <summary>
+        /// Project's authors.
+        /// </summary>
+        string Authors { get; }
+
+        /// <summary>
+        /// Project's name.
+        /// </summary>
+        string Product { get; }
+
+        /// <summary>
+        /// Project's version.
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// Project's website url.
+        /// </summary>
+        string ProjectUrl { get; }
+
+        /// <summary>
+        /// Project's support url.
+        /// </summary>
+        string HelpUrl { get; }
+    }
+}
