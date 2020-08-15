ac888f2fe6cfb55a13d1c5e2876210ac92369cac
Kir-Antipov <kp_antipov@mail.ru>
2020-08-15 08:49:45 +0300

    Made "About" form

---
+++ src/MonkeyTyper.WinForms/Forms/AboutForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace MonkeyTyper.WinForms.Forms
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.logoPicture = new System.Windows.Forms.PictureBox();
+            this.infoText = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoPicture
+            // 
+            this.logoPicture.Image = global::MonkeyTyper.WinForms.Properties.Resources.icon;
+            this.logoPicture.Location = new System.Drawing.Point(13, 13);
+            this.logoPicture.Name = "logoPicture";
+            this.logoPicture.Size = new System.Drawing.Size(256, 256);
+            this.logoPicture.TabIndex = 0;
+            this.logoPicture.TabStop = false;
+            // 
+            // infoText
+            // 
+            this.infoText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.infoText.BackColor = System.Drawing.SystemColors.Control;
+            this.infoText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.infoText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.infoText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.infoText.Location = new System.Drawing.Point(276, 15);
+            this.infoText.Name = "infoText";
+            this.infoText.ReadOnly = true;
+            this.infoText.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.infoText.Size = new System.Drawing.Size(385, 252);
+            this.infoText.TabIndex = 1;
+            this.infoText.Text = "";
+            this.infoText.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.InfoText_LinkClicked);
+            this.infoText.Enter += new System.EventHandler(this.InfoText_Focus);
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(673, 280);
+            this.Controls.Add(this.infoText);
+            this.Controls.Add(this.logoPicture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About MonkeyTyper";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoPicture;
+        private System.Windows.Forms.RichTextBox infoText;
+    }
+}
---
+++ src/MonkeyTyper.WinForms/Forms/AboutForm.cs
@@ -0,0 +1,38 @@
+﻿using MonkeyTyper.WinForms.Helpers;
+using MonkeyTyper.WinForms.Data;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MonkeyTyper.WinForms.Forms
+{
+    public partial class AboutForm : Form
+    {
+        #region Init
+        public AboutForm(IProjectProperties properties)
+        {
+            InitializeComponent();
+
+            infoText
+                .Append(properties.Product, Color.FromArgb(232, 178, 103))
+                .AppendLine($", {(properties.Version.StartsWith("v", StringComparison.InvariantCultureIgnoreCase) ? string.Empty : "v")}{properties.Version}")
+                .AppendLine()
+                .AppendLine(properties.Copyright)
+                .AppendLine("All Rights Reserved.")
+                .AppendLine()
+                .AppendLine("Visit our website to learn more about the product:")
+                .AppendLine(properties.ProjectUrl)
+                .AppendLine()
+                .AppendLine("If you need help, visit this link:")
+                .AppendLine(properties.HelpUrl);
+        }
+        #endregion
+
+        #region Handlers
+        private void InfoText_Focus(object sender, EventArgs e) => logoPicture.Focus();
+
+        private void InfoText_LinkClicked(object sender, LinkClickedEventArgs e) => Process.Start(e.LinkText);
+        #endregion
+    }
+}
