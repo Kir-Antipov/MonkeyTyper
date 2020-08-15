e1962af09cab2a7cd6c5bdf126e0cacf6907247b
Kir-Antipov <kp_antipov@mail.ru>
2020-08-15 11:02:41 +0300

    Added method to cast ServiceDescriptor to DI's ServiceDescriptor

---
+++ src/MonkeyTyper.WinForms/Helpers/ServiceDescriptorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using MonkeyTyperServiceDescriptor = MonkeyTyper.Core.Plugins.ServiceDescriptor;
+using MicrosoftServiceDescriptor = Microsoft.Extensions.DependencyInjection.ServiceDescriptor;
+using ServiceLifetime = Microsoft.Extensions.DependencyInjection.ServiceLifetime;
+using System;
+
+namespace MonkeyTyper.WinForms.Helpers
+{
+    internal static class ServiceDescriptorExtensions
+    {
+        public static MicrosoftServiceDescriptor Transform(this MonkeyTyperServiceDescriptor serviceDescriptor) => serviceDescriptor switch
+        {
+            { ImplementationInstance: { } } => new MicrosoftServiceDescriptor(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationInstance),
+            { ImplementationFactory: { } } => new MicrosoftServiceDescriptor(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationFactory, (ServiceLifetime)(int)serviceDescriptor.Lifetime),
+            { } => new MicrosoftServiceDescriptor(serviceDescriptor.ServiceType, serviceDescriptor.ImplementationType, (ServiceLifetime)(int)serviceDescriptor.Lifetime),
+            _ => throw new ArgumentNullException(nameof(serviceDescriptor))
+        };
+    }
+}
