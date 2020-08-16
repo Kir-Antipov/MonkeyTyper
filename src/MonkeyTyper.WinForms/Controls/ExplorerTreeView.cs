2c79f52701f321ae8954aae5bc79b63526bfac68
Kir-Antipov <kp_antipov@mail.ru>
2020-08-16 07:45:10 +0300

    Added explorer-styled TreeView.

---
+++ src/MonkeyTyper.WinForms/Controls/ExplorerTreeView.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace System.Windows.Forms
+{
+    /// <summary>
+    /// Explorer-styled TreeView.
+    /// </summary>
+    public class ExplorerTreeView : TreeView
+    {
+        [DllImport("uxtheme.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        private static extern int SetWindowTheme(IntPtr hwnd, string pszSubAppName, string? pszSubIdList);
+
+        /// <inheritdoc/>
+        protected override void OnParentVisibleChanged(EventArgs e)
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetWindowTheme(Handle, "explorer", null);
+
+            base.OnParentVisibleChanged(e);
+        }
+    }
+}
