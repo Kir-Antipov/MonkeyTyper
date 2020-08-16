21e548c8fe75ee55d9c9d914994ed3f7f2e21243
Kir-Antipov <kp_antipov@mail.ru>
2020-08-16 07:54:16 +0300

    Implemented settings form

---
+++ src/MonkeyTyper.WinForms/Forms/SettingsForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MonkeyTyper.WinForms.Forms
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.sections = new System.Windows.Forms.ExplorerTreeView();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.okButton.FlatAppearance.BorderSize = 2;
+            this.okButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(247)))));
+            this.okButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(241)))), ((int)(((byte)(251)))));
+            this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.okButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.okButton.Location = new System.Drawing.Point(568, 403);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(98, 33);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(173)))), ((int)(((byte)(173)))));
+            this.cancelButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(228)))), ((int)(((byte)(247)))));
+            this.cancelButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(241)))), ((int)(((byte)(251)))));
+            this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cancelButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(672, 403);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(98, 33);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            // 
+            // sections
+            // 
+            this.sections.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.sections.BackColor = System.Drawing.SystemColors.Control;
+            this.sections.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sections.FullRowSelect = true;
+            this.sections.Indent = 20;
+            this.sections.Location = new System.Drawing.Point(12, 12);
+            this.sections.Name = "sections";
+            this.sections.ShowLines = false;
+            this.sections.Size = new System.Drawing.Size(250, 385);
+            this.sections.TabIndex = 1;
+            this.sections.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.Sections_AfterSelect);
+            // 
+            // SettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(782, 448);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.sections);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SettingsForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MonkeyTyper Settings";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ExplorerTreeView sections;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/MonkeyTyper.WinForms/Forms/SettingsForm.cs
@@ -0,0 +1,217 @@
+﻿using MonkeyTyper.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MonkeyTyper.WinForms.Forms
+{
+    /// <summary>
+    /// Gives a person the ability to change the settings
+    /// provided by the <see cref="ISettingsProvider"/>.
+    /// </summary>
+    public partial class SettingsForm : Form
+    {
+        #region Var
+        private ISettingsProvider SettingsProvider { get; }
+
+        private Dictionary<TreeNode, DataGridView> SettingsViews { get; }
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Initialize a new instance of the <see cref="SettingsForm"/> class.
+        /// </summary>
+        /// <param name="settingsProvider">
+        /// The <see cref="ISettingsProvider"/> instance.
+        /// </param>
+        public SettingsForm(ISettingsProvider settingsProvider)
+        {
+            SettingsProvider = settingsProvider ?? throw new ArgumentNullException(nameof(settingsProvider));
+            SettingsViews = new Dictionary<TreeNode, DataGridView>();
+
+            InitializeComponent();
+        }
+
+        private void InitializeOkButton(Button button)
+        {
+            const int width = 2;
+
+            button.FlatAppearance.BorderSize = width;
+            button.FlatAppearance.CheckedBackColor = button.BackColor;
+            button.MouseDown += (s, e) => button.FlatAppearance.BorderSize = width / 2;
+            button.MouseUp += (s, e) => sections.Focus();
+            button.MouseEnter += (s, e) => button.FlatAppearance.BorderSize = width / 2;
+            button.MouseLeave += (s, e) => button.FlatAppearance.BorderSize = width;
+        }
+
+        private void InitializeCancelButton(Button button)
+        {
+            const int width = 1;
+            Color defaultColor = Color.FromArgb(173, 173, 173);
+            Color hoverColor = Color.FromArgb(0, 120, 215);
+            Color downColor = Color.FromArgb(0, 84, 153);
+
+            button.FlatAppearance.BorderSize = width;
+            button.FlatAppearance.BorderColor = defaultColor;
+            button.FlatAppearance.CheckedBackColor = button.BackColor;
+            button.MouseDown += (s, e) => { button.FlatAppearance.BorderSize = width; button.FlatAppearance.BorderColor = downColor; };
+            button.MouseUp += (s, e) => sections.Focus();
+            button.MouseEnter += (s, e) => { button.FlatAppearance.BorderSize = width; button.FlatAppearance.BorderColor = hoverColor; };
+            button.MouseLeave += (s, e) => { button.FlatAppearance.BorderSize = width; button.FlatAppearance.BorderColor = defaultColor; };
+        }
+
+        private void InitializeSettings(TreeView sections, ISettingsProvider provider)
+        {
+            foreach (ISettings settings in provider.Settings)
+            {
+                TreeNode root = sections.Nodes.Add(settings.DisplayName);
+                InitializeSettingsSection(root, settings, "General");
+                if (root.Nodes.Count == 0)
+                    sections.Nodes.Remove(root);
+            }
+
+            if (sections.Nodes.Count > 0)
+                sections.SelectedNode = sections.Nodes[0];
+        }
+
+        private void InitializeSettingsSection(TreeNode rootNode, ISettings section, string generalName)
+        {
+            TreeNode node = rootNode.Nodes.Add(generalName);
+
+            foreach (KeyValuePair<string, object?> entry in section.Where(x => x.Value is ISettings))
+            {
+                ISettings innerSettings = (ISettings)entry.Value!;
+                TreeNode subNode = rootNode.Nodes.Add(section.GetDisplayName(entry.Key));
+                InitializeSettingsSection(subNode, innerSettings, innerSettings.DisplayName);
+                if (subNode.Nodes.Count == 0)
+                    rootNode.Nodes.Remove(subNode);
+            }
+
+            DataGridView grid = CreateDefaultDataGridView();
+            grid.Tag = section;
+            foreach (KeyValuePair<string, object?> entry in section.Where(x => !(x.Value is ISettings)))
+                grid.Rows.Add(new DataGridViewRow
+                {
+                    Cells =
+                    {
+                        new DataGridViewTextBoxCell { Value = entry.Key },
+                        new DataGridViewTextBoxCell { Value = section.GetDisplayName(entry.Key) },
+                        CreateValueCell(entry.Value)
+                    }
+                });
+
+            if (grid.Rows.Count > 0)
+            {
+                Controls.Add(grid);
+                SettingsViews.Add(node, grid);
+                grid.CellEndEdit += Cell_Edited;
+            }
+            else if (node.Nodes.Count == 0)
+            {
+                rootNode.Nodes.Remove(node);
+            }
+        }
+        #endregion
+
+        #region Functions
+        private DataGridView CreateDefaultDataGridView() => new DataGridView
+        {
+            Columns =
+            {
+                new DataGridViewColumn(new DataGridViewTextBoxCell()) { Visible = false, Name = "Name", ReadOnly = true },
+                new DataGridViewColumn(new DataGridViewTextBoxCell()) { Name = "DisplayName", ReadOnly = true },
+                new DataGridViewColumn(new DataGridViewTextBoxCell()) { Name = "Value" }
+            },
+            BackgroundColor = SystemColors.Control,
+            Left = sections.Right + 6,
+            Top = 12,
+            Width = ClientSize.Width - sections.Width - sections.Left - 6 - 12,
+            Height = sections.Height,
+            RowHeadersVisible = false,
+            AllowUserToAddRows = false,
+            AllowUserToDeleteRows = false,
+            AllowUserToOrderColumns = false,
+            AllowUserToResizeColumns = false,
+            AllowUserToResizeRows = false,
+            AllowDrop = false,
+            ColumnHeadersVisible = false,
+            AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill,
+            MultiSelect = false,
+            ShowCellErrors = true,
+            ShowEditingIcon = true,
+            SelectionMode = DataGridViewSelectionMode.FullRowSelect,
+            Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right
+        };
+
+        private static DataGridViewCell CreateValueCell(object? cell) => cell switch
+        {
+            bool _ => new DataGridViewCheckBoxCell { Value = cell, ValueType = typeof(bool) },
+            Enum e => CreateEnumCell(e), 
+            _ => new DataGridViewTextBoxCell { Value = cell, ValueType = typeof(string) }
+        };
+
+        private static DataGridViewCell CreateEnumCell(Enum value)
+        {
+            DataGridViewComboBoxCell cell = new DataGridViewComboBoxCell
+            { 
+                Value = value,
+                ValueType = value.GetType()
+            };
+
+            cell.Items.AddRange(Enum.GetValues(cell.ValueType).Cast<object>().ToArray());
+
+            return cell;
+        }
+        #endregion
+
+        #region Handlers
+        protected override void OnShown(EventArgs e)
+        {
+            foreach (DataGridView grid in SettingsViews.Values)
+            {
+                Controls.Remove(grid);
+                grid.Dispose();
+            }
+            SettingsViews.Clear();
+            sections.Nodes.Clear();
+
+            InitializeSettings(sections, SettingsProvider);
+            InitializeOkButton(okButton);
+            InitializeCancelButton(cancelButton);
+            base.OnShown(e);
+        }
+
+        private void Cell_Edited(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!(sender is DataGridView { Tag: ISettings settings } grid))
+                return;
+
+            DataGridViewCell cell = grid.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            string name = (string)grid.Rows[e.RowIndex].Cells["Name"].Value;
+            try
+            {
+                settings[name] = cell.Value;
+                cell.ErrorText = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                while (ex.InnerException is { } inner && !string.IsNullOrEmpty(inner.Message))
+                    ex = inner;
+
+                MessageBox.Show(this, ex.Message, $"Value \"{cell.Value}\" is not valid for this field", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                cell.ErrorText = ex.Message;
+                cell.Value = settings[name];
+            }
+        }
+
+        private void Sections_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (SettingsViews.TryGetValue(e.Node, out DataGridView view) || e.Node.FirstNode is { } && SettingsViews.TryGetValue(e.Node.FirstNode, out view))
+                view.BringToFront();
+        }
+        #endregion
+    }
+}
