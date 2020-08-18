605a6a4bf9d9f0a5700b0a6fb4b7b4d35c57168d
Kir-Antipov <kp_antipov@mail.ru>
2020-08-18 19:52:14 +0300

    Configured program services

--- src/MonkeyTyper.WinForms/Program.cs
+++ src/MonkeyTyper.WinForms/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MonkeyTyper.WinForms.Forms;
+using MonkeyTyper.WinForms.Data;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using MonkeyTyper.Core.Format;
+using MonkeyTyper.Core.Mail;
+using MonkeyTyper.Core.Plugins;
+using MonkeyTyper.WinForms.Helpers;
 
 namespace MonkeyTyper.WinForms
 {
@@ -47,7 +55,9 @@ namespace MonkeyTyper.WinForms
         {
             IServiceCollection services = new ServiceCollection();
             ConfigureServices(services);
+            ConfigurePlugins(services);
             ConfigureEntryForm(services);
+            ConfigureAppSettings(services);
             return services.BuildServiceProvider();
         }
 
@@ -57,7 +67,45 @@ namespace MonkeyTyper.WinForms
         /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
         private static void ConfigureServices(IServiceCollection services)
         {
+            IEnumerable<Type> forms = Assembly.GetExecutingAssembly()
+                .ExportedTypes
+                .Where(typeof(Form).IsAssignableFrom)
+                .Where(x => !x.IsAbstract);
 
+            foreach (Type form in forms)
+                services.AddScoped(form);
+
+            services
+                .AddScoped<IProjectProperties, AssemblyProjectProperties>()
+                .AddScoped<IStringFormatter, DefaultStringFormatter>()
+                .AddScoped<IMailClient, DefaultMailClient>()
+                .AddScoped<IMessageBuilder, DefaultMessageBuilder>()
+                .AddScoped<IPluginManager, DefaultPluginManager>()
+                .AddSingleton<ISettingsProvider>(new DefaultSettingsProvider());
+        }
+
+        /// <summary>
+        /// Loads all plugins to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
+        /// <TODO>
+        /// Add the ability to load a third party <see cref="IPluginManager"/>.
+        /// </TODO>
+        private static void ConfigurePlugins(IServiceCollection services)
+        {
+            IServiceProvider provider = services.BuildServiceProvider();
+            IPluginManager manager = provider.GetRequiredService<IPluginManager>();
+            ISettingsProvider settingsProvider = provider.GetRequiredService<ISettingsProvider>();
+
+            foreach (var descriptor in manager.Load())
+            {
+                services.Add(descriptor.Transform());
+                if (descriptor.SettingsType is { } settingsType)
+                {
+                    settingsProvider.Register(settingsType);
+                    services.AddScoped(settingsType, provider => provider.GetRequiredService<ISettingsProvider>().Get(settingsType));
+                }
+            }
         }
 
         /// <summary>
@@ -66,5 +114,17 @@ namespace MonkeyTyper.WinForms
         /// </summary>
         /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
         private static void ConfigureEntryForm(IServiceCollection services) => services.AddScoped<Form, MainForm>();
+
+        /// <summary>
+        /// Adds <see cref="AppSettings"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> instance.</param>
+        private static void ConfigureAppSettings(IServiceCollection services)
+        {
+            IServiceProvider provider = services.BuildServiceProvider();
+            ISettingsProvider settingsProvider = provider.GetRequiredService<ISettingsProvider>();
+            settingsProvider.RegisterAppSettings(typeof(AppSettings));
+            services.AddScoped(typeof(AppSettings), provider => provider.GetRequiredService<ISettingsProvider>().Get(typeof(AppSettings)));
+        }
     }
 }
