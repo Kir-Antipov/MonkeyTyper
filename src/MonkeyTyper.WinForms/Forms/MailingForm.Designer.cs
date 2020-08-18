2f2d04a104d59c7c52a8fcbba0499f08cd39f697
Kir-Antipov <kp_antipov@mail.ru>
2020-08-18 21:09:33 +0300

    Created form that describes mailing progress

---
+++ src/MonkeyTyper.WinForms/Forms/MailingForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MonkeyTyper.WinForms.Forms
+{
+    partial class MailingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MailingForm));
+            this.messageProgressLabel = new System.Windows.Forms.Label();
+            this.messageSendingProgress = new System.Windows.Forms.ProgressBar();
+            this.totalProgressLabel = new System.Windows.Forms.Label();
+            this.totalProgress = new System.Windows.Forms.ProgressBar();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageProgressLabel
+            // 
+            this.messageProgressLabel.AutoSize = true;
+            this.messageProgressLabel.Location = new System.Drawing.Point(12, 9);
+            this.messageProgressLabel.Name = "messageProgressLabel";
+            this.messageProgressLabel.Size = new System.Drawing.Size(187, 20);
+            this.messageProgressLabel.TabIndex = 0;
+            this.messageProgressLabel.Text = "Message sending progress:";
+            // 
+            // messageSendingProgress
+            // 
+            this.messageSendingProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.messageSendingProgress.Location = new System.Drawing.Point(16, 32);
+            this.messageSendingProgress.Name = "messageSendingProgress";
+            this.messageSendingProgress.Size = new System.Drawing.Size(604, 23);
+            this.messageSendingProgress.Step = 1;
+            this.messageSendingProgress.TabIndex = 1;
+            // 
+            // totalProgressLabel
+            // 
+            this.totalProgressLabel.AutoSize = true;
+            this.totalProgressLabel.Location = new System.Drawing.Point(16, 73);
+            this.totalProgressLabel.Name = "totalProgressLabel";
+            this.totalProgressLabel.Size = new System.Drawing.Size(106, 20);
+            this.totalProgressLabel.TabIndex = 2;
+            this.totalProgressLabel.Text = "Total progress:";
+            // 
+            // totalProgress
+            // 
+            this.totalProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.totalProgress.Location = new System.Drawing.Point(16, 96);
+            this.totalProgress.Name = "totalProgress";
+            this.totalProgress.Size = new System.Drawing.Size(604, 23);
+            this.totalProgress.Step = 1;
+            this.totalProgress.TabIndex = 3;
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pauseButton.AutoSize = true;
+            this.pauseButton.Location = new System.Drawing.Point(364, 125);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(125, 30);
+            this.pauseButton.TabIndex = 4;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelButton.AutoSize = true;
+            this.cancelButton.Location = new System.Drawing.Point(495, 125);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(125, 30);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // MailingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(632, 165);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.totalProgress);
+            this.Controls.Add(this.totalProgressLabel);
+            this.Controls.Add(this.messageSendingProgress);
+            this.Controls.Add(this.messageProgressLabel);
+            this.Controls.Add(this.pauseButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MailingForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Sending messages...";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageProgressLabel;
+        private System.Windows.Forms.ProgressBar messageSendingProgress;
+        private System.Windows.Forms.Label totalProgressLabel;
+        private System.Windows.Forms.ProgressBar totalProgress;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/MonkeyTyper.WinForms/Forms/MailingForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MonkeyTyper.WinForms.Forms
+{
+    /// <summary>
+    /// Proxy form providing information
+    /// about the mailing progress.
+    /// </summary>
+    public partial class MailingForm : Form
+    {
+        #region Var
+        /// <summary>
+        /// Occurs when a process is requested to pause.
+        /// </summary>
+        public event EventHandler? PauseRequest;
+
+        /// <summary>
+        /// Occurs when a process is requested to be canceled.
+        /// </summary>
+        public event EventHandler? CancellationRequest;
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Initialize a new instance of the <see cref="MailingForm"/> class.
+        /// </summary>
+        public MailingForm() => InitializeComponent();
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Removes all handlers from control events.
+        /// </summary>
+        public virtual void ResetControlEvents()
+        {
+            PauseRequest = null;
+            CancellationRequest = null;
+        }
+
+        /// <summary>
+        /// Informs about the progress of message sending.
+        /// </summary>
+        /// <param name="bytesTransferred">The number of bytes transferred.</param>
+        /// <param name="totalSize">The total size, in bytes, of the message being transferred.</param>
+        /// <param name="tip">Description of the current process.</param>
+        public virtual void SetMessageProgress(long bytesTransferred, long totalSize, string? tip = null) => 
+            SetProgress(messageProgressLabel, messageSendingProgress, bytesTransferred, totalSize, tip);
+
+        /// <summary>
+        /// Informs about the overall progress of the process.
+        /// </summary>
+        /// <param name="emailIndex">One-base ordinal number of the sent message.</param>
+        /// <param name="totalEmails">The total number of messages to send.</param>
+        /// <param name="tip">Description of the current process.</param>
+        public virtual void SetTotalProgress(int emailIndex, int totalEmails, string? tip = null) => 
+            SetProgress(totalProgressLabel, totalProgress, emailIndex, totalEmails, tip);
+
+        private void SetProgress(Label label, ProgressBar progress, long valueLong, long totalLong, string? tip)
+        {
+            int value = (int)(valueLong > int.MaxValue ? int.MaxValue : valueLong);
+            int total = (int)(totalLong > int.MaxValue ? int.MaxValue : totalLong);
+            ProgressBarStyle style = ProgressBarStyle.Blocks;
+            if (total < 0)
+            {
+                total = int.MaxValue;
+                style = ProgressBarStyle.Marquee;
+            }
+
+            label.Text = $"{label.Text.Substring(0, label.Text.IndexOf(':') + 1)} {tip ?? string.Empty}";
+                
+            if ((progress.Maximum, progress.Style) != (total, style))
+                (progress.Maximum, progress.Style) = (total, style);
+            progress.Value = Math.Min(progress.Maximum, value);
+        }
+        #endregion
+
+        #region Handlers
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure you want to pause the process?", "Confirmation required.", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                PauseRequest?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "Are you sure you want to stop the process? This operation cannot be undone.", "Confirmation required.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                CancellationRequest?.Invoke(this, EventArgs.Empty);
+        }
+        #endregion
+    }
+}
