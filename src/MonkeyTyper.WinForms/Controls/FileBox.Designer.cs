9015cd6664044aba38804c242cd54e604f9f8c48
Kir-Antipov <kp_antipov@mail.ru>
2020-08-18 19:42:27 +0300

    Implemented control for file preview.

---
+++ src/MonkeyTyper.WinForms/Controls/FileBox.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MonkeyTyper.WinForms.Controls
+{
+    partial class FileBox
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filenameLabel = new System.Windows.Forms.Label();
+            this.icon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // filenameLabel
+            // 
+            this.filenameLabel.AutoSize = true;
+            this.filenameLabel.Location = new System.Drawing.Point(41, 10);
+            this.filenameLabel.Name = "filenameLabel";
+            this.filenameLabel.Size = new System.Drawing.Size(0, 17);
+            this.filenameLabel.TabIndex = 1;
+            this.filenameLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Control_MouseClick);
+            // 
+            // icon
+            // 
+            this.icon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.icon.Location = new System.Drawing.Point(3, 3);
+            this.icon.Name = "icon";
+            this.icon.Size = new System.Drawing.Size(32, 32);
+            this.icon.TabIndex = 0;
+            this.icon.TabStop = false;
+            this.icon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Control_MouseClick);
+            // 
+            // FileBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.filenameLabel);
+            this.Controls.Add(this.icon);
+            this.Name = "FileBox";
+            this.Size = new System.Drawing.Size(150, 39);
+            ((System.ComponentModel.ISupportInitialize)(this.icon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox icon;
+        private System.Windows.Forms.Label filenameLabel;
+    }
+}
---
+++ src/MonkeyTyper.WinForms/Controls/FileBox.cs
@@ -0,0 +1,155 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MonkeyTyper.WinForms.Controls
+{
+    /// <summary>
+    /// Provides a preview for the given file.
+    /// </summary>
+    public sealed partial class FileBox : UserControl
+    {
+        #region Var
+        /// <inheritdoc/>
+        public override bool AutoSize
+        { 
+            get => base.AutoSize;
+            set 
+            { 
+                base.AutoSize = filenameLabel.AutoSize = value; 
+                UpdateContainer(); 
+            } 
+        }
+
+        /// <inheritdoc/>
+        public override Font Font
+        { 
+            get => base.Font;
+            set
+            {
+                base.Font = filenameLabel.Font = value;
+                UpdateContainer();
+            }
+        }
+
+        /// <summary>
+        /// Icon location.
+        /// </summary>
+        [Category("Layout")]
+        [Description("Icon location.")]
+        public Point IconLocation
+        {
+            get => icon.Location;
+            set
+            {
+                icon.Location = value;
+                UpdateContainer();
+            }
+        }
+
+        /// <summary>
+        /// Icon anchor.
+        /// </summary>
+        [Category("Layout")]
+        [Description("Icon anchor.")]
+        public AnchorStyles IconAnchor
+        {
+            get => icon.Anchor; 
+            set 
+            { 
+                icon.Anchor = value; 
+                UpdateContainer(); 
+            }
+        }
+
+        /// <summary>
+        /// Icon size.
+        /// </summary>
+        [Category("Layout")]
+        [Description("Icon size.")]
+        public Size IconSize
+        {
+            get => icon.Size; 
+            set 
+            {
+                icon.Size = value;
+                UpdateContainer();
+            }
+        }
+
+        /// <summary>
+        /// Specifies whether to show only the filename (<see langword="true"/>)
+        /// or its full path (<see langword="false"/>).
+        /// </summary>
+        [Category("Appearance")]
+        [Description("Specifies whether to show only the filename or its full path.")]
+        public bool ShowOnlyFilename
+        {
+            get => _showOnlyFilename;
+            set
+            {
+                _showOnlyFilename = value;
+                UpdateContainer();
+            }
+        }
+        private bool _showOnlyFilename = true;
+
+        /// <summary>
+        /// A relative or absolute path to the file.
+        /// </summary>
+        [Category("Appearance")]
+        [Description("A relative or absolute path to the file.")]
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                _path = value ?? string.Empty;
+                UpdateContainer();
+            }
+        }
+        private string _path = string.Empty;
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Initialize a new instance of the <see cref="FileBox"/> class.
+        /// </summary>
+        public FileBox() => InitializeComponent();
+        #endregion
+
+        #region Functions
+        private void UpdateContainer()
+        {
+            filenameLabel.Text = _showOnlyFilename ? System.IO.Path.GetFileName(_path) : _path;
+            icon.BackgroundImage?.Dispose();
+            Icon fileIcon;
+            try
+            {
+                fileIcon = File.Exists(_path) ? Icon.ExtractAssociatedIcon(_path) : SystemIcons.Error;
+            }
+            catch
+            {
+                fileIcon = SystemIcons.Error;
+            }
+            icon.BackgroundImage = fileIcon.ToBitmap();
+            fileIcon.Dispose();
+
+            if (AutoSize)
+            {
+                Height = 3 + icon.Height + 3;
+                Width = 3 + icon.Width + 3 + filenameLabel.Width + 3;
+                icon.Left = 3;
+                icon.Top = 3;
+                filenameLabel.Top = (Height - filenameLabel.Height) / 2;
+                filenameLabel.Left = icon.Right + 3;
+            }
+        }
+        #endregion
+
+        #region Handler
+        private void Control_MouseClick(object sender, MouseEventArgs e) => OnMouseClick(e);
+        #endregion
+    }
+}
