c19e6bb8b71ef7b1918452c5c1cb117811cfc608
Kir-Antipov <kp_antipov@mail.ru>
2020-08-18 19:27:45 +0300

    Implemented EventArgs for IMailClient.MessageProcessed

---
+++ src/MonkeyTyper.Core/Mail/MessageProcessedEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using MimeKit;
+using System;
+
+namespace MonkeyTyper.Core.Mail
+{
+    /// <summary>
+    /// The <see cref="EventArgs"/> for <see cref="IMailClient.MessageProcessed"/>.
+    /// </summary>
+    public class MessageProcessedEventArgs : EventArgs
+    {
+        #region Var
+        /// <summary>
+        /// The message.
+        /// </summary>
+        public MimeMessage Message { get; }
+
+        /// <summary>
+        /// Zero-based message ordinal.
+        /// </summary>
+        public int Ordinal { get; }
+
+        /// <summary>
+        /// The total number of messages to be
+        /// sent during the current iteration.
+        /// </summary>
+        /// <remarks>
+        /// If the total number of messages isn't known,
+        /// the value of this property will be -1.
+        /// </remarks>
+        public int Count { get; }
+
+        /// <summary>
+        /// Indicates whether the message was sent or skipped.
+        /// </summary>
+        public ProcessedType ProcessedType { get; }
+        #endregion
+
+        #region Init
+        /// <summary>
+        /// Initialize a new instance of the <see cref="MessageProcessedEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="ordinal">Zero-based message ordinal.</param>
+        /// <param name="count">
+        /// The total number of messages to be
+        /// sent by the calling method.
+        /// </param>
+        /// <param name="processedType">Indicates whether the message was sent or skipped.</param>
+        public MessageProcessedEventArgs(MimeMessage message, int ordinal, int count, ProcessedType processedType)
+        {
+            Message = message ?? throw new ArgumentNullException(nameof(message));
+            Count = count >= -1 ? count : throw new ArgumentOutOfRangeException(nameof(count));
+            Ordinal = ordinal >= 0 ? ordinal : throw new ArgumentOutOfRangeException(nameof(ordinal));
+            ProcessedType = processedType;
+        }
+        #endregion
+    }
+}
---
+++ src/MonkeyTyper.Core/Mail/ProcessedType.cs
@@ -0,0 +1,18 @@
+﻿namespace MonkeyTyper.Core.Mail
+{
+    /// <summary>
+    /// Indicates whether the message was sent or skipped.
+    /// </summary>
+    public enum ProcessedType
+    {
+        /// <summary>
+        /// Message was skipped.
+        /// </summary>
+        Skipped,
+
+        /// <summary>
+        /// Message was sent.
+        /// </summary>
+        Sent
+    }
+}
